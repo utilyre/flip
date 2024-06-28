101e2f390714e43840b1f3a61b6e0c2e021c9474
Amirabbas Fazelinia <utilyre@gmail.com>
2024-06-28 12:12:19 +0330

    fix(main): dynamically decide how many pipes fit the screen

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 #include <raylib.h>
 
 #include <assets.h>
@@ -22,8 +23,9 @@ int main()
   const float HOLE_SIZE = 250.0f;
   const float HOLE_PADDING = 100.0f;
 
-  Vector2 pipes[3];
-  for (size_t i = 0; i < 3; i++)
+  size_t pipes_len = ceilf((WINDOW_WIDTH - assets.pipe_above.width) / (PIPE_SPACING + assets.pipe_above.width)) + 1;
+  Vector2 pipes[pipes_len];
+  for (size_t i = 0; i < pipes_len; i++)
   {
     pipes[i].x = WINDOW_WIDTH + i * (PIPE_SPACING + assets.pipe_above.width);
     pipes[i].y = HOLE_PADDING + rand() % (int)(WINDOW_HEIGHT - HOLE_SIZE - 2.0f * HOLE_PADDING - assets.base.height);
@@ -34,12 +36,12 @@ int main()
   {
     BirdUpdate(&player);
 
-    for (size_t i = 0; i < 3; i++)
+    for (size_t i = 0; i < pipes_len; i++)
     {
       Vector2* pipe = &pipes[i];
       pipe->x += PIPE_VEL_X * GetFrameTime();
       if (pipe->x <= -assets.pipe_above.width) {
-        pipe->x += 3 * (assets.pipe_above.width + PIPE_SPACING);
+        pipe->x += pipes_len * (assets.pipe_above.width + PIPE_SPACING);
         pipe->y = rand() % (int)(WINDOW_HEIGHT - HOLE_SIZE - assets.base.height - 2.0f * HOLE_PADDING) + HOLE_PADDING;
       }
     }
@@ -48,7 +50,7 @@ int main()
 
     DrawTexture(assets.background, 0, 0, WHITE);
 
-    for (size_t i = 0; i < 3; i++)
+    for (size_t i = 0; i < pipes_len; i++)
     {
       Vector2 pipe = pipes[i];
       DrawTexture(assets.pipe_above, pipe.x, pipe.y - assets.pipe_above.height, WHITE);
