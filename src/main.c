94ea46ec399dc246e42c2d052e85076bb6899853
Amirabbas Fazelinia <utilyre@gmail.com>
2024-06-28 11:32:11 +0330

    feat(main): extend to three moving pipes

--- src/main.c
+++ src/main.c
@@ -17,12 +17,24 @@ int main()
   Assets assets = LoadAssets();
   Bird player = NewBird(assets);
 
+  const float PIPE_SPACING = 400.0f;
   const float PIPE_VEL_X = -250.0f;
   const float HOLE_SIZE = 250.0f;
   const float HOLE_PADDING = 100.0f;
-  Vector2 hole = {
-    WINDOW_WIDTH,
-    rand() % (int)(WINDOW_HEIGHT - HOLE_SIZE - assets.base.height - 2.0f * HOLE_PADDING) + HOLE_PADDING,
+
+  Vector2 pipes[] = {
+    {
+      WINDOW_WIDTH,
+      rand() % (int)(WINDOW_HEIGHT - HOLE_SIZE - assets.base.height - 2.0f * HOLE_PADDING) + HOLE_PADDING,
+    },
+    {
+      WINDOW_WIDTH + PIPE_SPACING + assets.pipe_above.width,
+      rand() % (int)(WINDOW_HEIGHT - HOLE_SIZE - assets.base.height - 2.0f * HOLE_PADDING) + HOLE_PADDING,
+    },
+    {
+      WINDOW_WIDTH + 2.0f * (PIPE_SPACING + assets.pipe_above.width),
+      rand() % (int)(WINDOW_HEIGHT - HOLE_SIZE - assets.base.height - 2.0f * HOLE_PADDING) + HOLE_PADDING,
+    },
   };
 
   SetTargetFPS(60);
@@ -30,18 +42,26 @@ int main()
   {
     BirdUpdate(&player);
 
-    hole.x += PIPE_VEL_X * GetFrameTime();
-    if (hole.x <= -assets.pipe_above.width) {
-      hole.x = GetScreenWidth();
-      hole.y = rand() % (int)(WINDOW_HEIGHT - HOLE_SIZE - assets.base.height - 2.0f * HOLE_PADDING) + HOLE_PADDING;
+    for (size_t i = 0; i < 3; i++)
+    {
+      Vector2* pipe = &pipes[i];
+      pipe->x += PIPE_VEL_X * GetFrameTime();
+      if (pipe->x <= -assets.pipe_above.width) {
+        pipe->x += 3 * (assets.pipe_above.width + PIPE_SPACING);
+        pipe->y = rand() % (int)(WINDOW_HEIGHT - HOLE_SIZE - assets.base.height - 2.0f * HOLE_PADDING) + HOLE_PADDING;
+      }
     }
 
     BeginDrawing();
 
     DrawTexture(assets.background, 0, 0, WHITE);
 
-    DrawTexture(assets.pipe_above, hole.x, hole.y - assets.pipe_above.height, WHITE);
-    DrawTexture(assets.pipe_below, hole.x, hole.y + HOLE_SIZE, WHITE);
+    for (size_t i = 0; i < 3; i++)
+    {
+      Vector2 pipe = pipes[i];
+      DrawTexture(assets.pipe_above, pipe.x, pipe.y - assets.pipe_above.height, WHITE);
+      DrawTexture(assets.pipe_below, pipe.x, pipe.y + HOLE_SIZE, WHITE);
+    }
 
     DrawTexture(assets.base, 0, WINDOW_HEIGHT - assets.base.height, WHITE);
 
